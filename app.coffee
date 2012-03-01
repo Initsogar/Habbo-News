5055756b9feb8fea8bcd06b5eaedce865b49f0a9
Rafael A. Agostini <initsogar@gmail.com>
2012-03-01 15:52:26 -0500

    Preparando los archivos para trabajar

---
+++ app.coffee
@@ -0,0 +1,22 @@
+express = require 'express'
+stylus = require 'stylus'
+
+app = express.createServer()
+ 
+app.use express.static(__dirname + '/public')
+
+console.log "#{__dirname}"
+
+app.use stylus.middleware
+	src: __dirname+'\views'
+	dest: __dirname+'\public\css'
+	compile: (str, path, fn) ->
+  	stylus(str).set('filename', path).set('compress', true)
+
+app.set 'view engine', 'jade'
+ 
+app.get '/', (request, response) ->
+  response.render 'index', { pageTitle: 'Express with Coffee' }
+ 
+app.listen 3000
+console.log "Express server listening on port %d", app.address().port
---
+++ app.js
@@ -0,0 +1,34 @@
+(function() {
+  var app, express, stylus;
+
+  express = require('express');
+
+  stylus = require('stylus');
+
+  app = express.createServer();
+
+  app.use(express.static(__dirname + '/public'));
+
+  console.log("" + __dirname);
+
+  app.use(stylus.middleware({
+    src: __dirname + '\views',
+    dest: __dirname + '\public\css',
+    compile: function(str, path, fn) {
+      return stylus(str).set('filename', path).set('compress', true);
+    }
+  }));
+
+  app.set('view engine', 'jade');
+
+  app.get('/', function(request, response) {
+    return response.render('index', {
+      pageTitle: 'Express with Coffee'
+    });
+  });
+
+  app.listen(3000);
+
+  console.log("Express server listening on port %d", app.address().port);
+
+}).call(this);
